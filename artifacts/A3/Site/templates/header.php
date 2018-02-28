70d221ca6fd9cc49dfb4ea18ded21514a231525d
mariosantosprivate <mariosantos.private@gmail.com>
2018-02-28 13:11:57 +0000

    navbar not terrible looking

--- artifacts/A3/Site/templates/header.php
+++ artifacts/A3/Site/templates/header.php
@@ -12,10 +12,11 @@
 <body>
 
 		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-				<a class="navbar-brand" href="#">P</a>
+			<a class="navbar-brand mr-auto" href="http://localhost/Site/index.php#"><img src="res/brand_icon.png" alt="Plenum" width=25%>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				  <span class="navbar-toggler-icon"></span>
 				</button>
+				</a>
 			  
 				<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				  <ul class="navbar-nav mr-auto">
@@ -32,14 +33,13 @@
 						<a class="nav-link" href="#">Dashboard</a>
 					</li>
 					<li>
-						<form class="form-inline my-2 my-lg-0">
-							<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-							<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+						<form class="form-inline">
+							<input class="form-control" type="search" placeholder="Search" aria-label="Search">
 						</form>
 					</li>
 				  	</ul>
 				 	 <div class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
 						  Authentification
 						</a>
 						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
