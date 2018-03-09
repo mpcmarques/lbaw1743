255ffdbdccf8b0342c087f06ad0e972de4ba6675
Bernardo Barbosa <up201503477@fe.up.pt>
2018-03-09 02:07:35 +0000

    refactor modals, add facebook and google buttons 💄♿️

--- website/resources/views/layouts/navbar.blade.php
+++ website/resources/views/layouts/navbar.blade.php
@@ -20,10 +20,10 @@
 
 			<div class="buttons text-center">
 				<button class="btn btn-outline-terciary my-2 my-sm-0" data-toggle="modal" data-target="#signup-modal">
-					Sign up
+					Register
 				</button>
 				<button class="btn btn-outline-primary my-2 my-sm-0"  data-toggle="modal" data-target="#signin-modal">
-					Sign in
+					Login
 				</button>
 			</div>
 		</div>
@@ -33,31 +33,42 @@
 		<div class="modal-dialog" role="document">
 			<div class="modal-content">
 				<div class="modal-header text-center bg-primary">
-					<h4 class="modal-title w-100 font-weight-bold">Sign In</h4>
+					<h4 class="modal-title w-100 font-weight-bold">Login</h4>
 					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 						<span aria-hidden="true">&times;</span>
 					</button>
 				</div>
 				<div class="modal-body mx-3">
-					<div class="md-form mb-5">
+					<div class="md-form mb-3">
 						<label data-error="wrong" data-success="right" for="defaultForm-email">Username / Email</label>
 						<input type="email" id="defaultForm-email" class="form-control validate">
 					</div>
 
-					<div class="md-form mb-4">
+					<div class="md-form mb-3">
 						<label data-error="wrong" data-success="right" for="defaultForm-pass">Password</label>
 						<input type="password" id="defaultForm-pass" class="form-control validate">
 						<small><a id="forgotPassword" href="forgot_password.php">Forgot Password?</a></small>
 					</div>
 
 				</div>
-				<div class="buttons">
-						<button type="submit" class="btn btn-primary">Sign In</button>
+				<div class="buttons text-center">
+						<button type="submit" class="btn btn-primary">Login</button>
 				</div>
 
 				<div class="modal-footer d-flex justify-content-center">
-					<h5 class="modal-title w-100 font-weight-bold">Sign in with</h5>
+					<div class="buttons">
+						<button class="btn facebook">
+							<img src="img/facebook.svg" alt="facebook" width="25">
+							Facebook
+						</button>
+						<button class="btn google">
+							<img src="img/google.png" alt="google" width="30">
+							Google+
+						</button>
+					</div>
 				</div>
+
+
 			</div>
 		</div>
 	</div>
@@ -66,37 +77,37 @@
 		<div class="modal-dialog modal-lg" role="document">
 			<div class="modal-content">
 				<div class="modal-header text-center bg-primary">
-					<h4 class="modal-title w-100 font-weight-bold">Sign Up</h4>
+					<h4 class="modal-title w-100 font-weight-bold">Register</h4>
 					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 						<span aria-hidden="true">&times;</span>
 					</button>
 				</div>
 				<div class="modal-body">
-					<div class="form-group row">
+					<div class="form-group row required">
 						<label for="inputName" class="col-sm-4 col-form-label">Full Name</label>
 						<div class="col-sm-8">
 							<input type="text" class="form-control" placeholder="Full Name" required>
 						</div>
 					</div>
-					<div class="form-group row">
+					<div class="form-group row required">
 						<label for="inputUsername" class="col-sm-4 col-form-label">Username</label>
 						<div class="col-sm-8">
 							<input type="text" class="form-control" placeholder="Username" required>
 						</div>
 					</div>
-					<div class="form-group row">
+					<div class="form-group row required">
 						<label for="inputEmail" class="col-sm-4 col-form-label">Email</label>
 						<div class="col-sm-8">
 							<input type="email" class="form-control" placeholder="Email" required>
 						</div>
 					</div>
-					<div class="form-group row">
+					<div class="form-group row required">
 						<label for="inputPassword" class="col-sm-4 col-form-label">Password</label>
 						<div class="col-sm-8">
 							<input type="password" class="form-control validate" placeholder="password" required>
 						</div>
 					</div>
-					<div class="form-group row">
+					<div class="form-group row required">
 						<label for="inputRepeatPassword" class="col-sm-4 col-form-label">Repeat Password</label>
 						<div class="col-sm-8">
 							<input type="password" class="form-control validate" placeholder="password" required>
@@ -109,7 +120,7 @@
 							<input type="text" class="form-control validate" placeholder="Institucion / Company">
 						</div>
 					</div>
-					<div class="form-group row">
+					<div class="form-group row required">
 						<label for="inputInstCompany" class="col-sm-4 col-form-label">Birthday</label>
 						<div class="col-sm-8">
 							<input type="date" class="form-control validate" required>
@@ -124,7 +135,7 @@
 								I read and agreed with the Terms of Service and the Privacy Policy
 							</label>
 						</div>
-						<button type="submit" class="btn btn-primary">Sign Up</button>
+						<button type="submit" class="btn btn-primary">Register</button>
 					</ul>
 				</div>
 			</div>
