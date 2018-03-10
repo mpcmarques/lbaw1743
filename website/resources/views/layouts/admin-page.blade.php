2897709cf668eb25ccb41cd85d2f207a5f72dfba
Mateus <mpcmarques@icloud.com>
2018-03-10 16:02:24 +0000

    css path as a variable

--- website/resources/views/layouts/admin-page.blade.php
+++ website/resources/views/layouts/admin-page.blade.php
@@ -1,5 +1,7 @@
 @extends ('layouts.page-layout')
 
+@section('css', 'css/admin.css')
+
 @section('header')
   @include('layouts.navbar_admin')
 @endsection
--- website/resources/views/layouts/app.blade.php
+++ website/resources/views/layouts/app.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.page-layout')
 
+@section('css', "css/css.css")
+
 @section('header')
     @include('layouts.navbar')
 @endsection
-
 
 @section('footer')
     @include('layouts.footer')
