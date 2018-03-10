6fc4eab4288fac0cab821112d88080247a7967ed
Mateus <mpcmarques@icloud.com>
2018-03-10 18:00:56 +0000

    fixed imports

--- website/resources/views/search/users_card.blade.php
+++ website/resources/views/search/users_card.blade.php
@@ -22,42 +22,30 @@
 
 @section('card-body')
 <div class="row">
-  <div class="col-3">
+  <div class="col">
     <a href="#">
       <div class="card">
-        <img src="images/profile_pic.png" class="card-img-top" alt="Profile Picture">
+        <img src="{{ asset('img/profile_pic.png')}}" class="card-img-top" alt="Profile Picture">
         <div class="card-body">
           <h5 class="card-title text-center">Name Lastname</h5>
         </div>
       </div>
     </a>
   </div>
-  <div class="col-3">
+  <div class="col">
     <a href="#">
       <div class="card">
-        <img src="images/profile_pic.png" class="card-img-top" alt="Profile Picture">
+        <img src="{{ asset('img/profile_pic.png')}}" class="card-img-top" alt="Profile Picture">
         <div class="card-body">
           <h5 class="card-title text-center">Name Lastname</h5>
         </div>
       </div>
     </a>
   </div>
-  <div class="col-3">
+  <div class="col">
     <a href="#">
       <div class="card">
-        <img src="images/profile_pic.png" class="card-img-top" alt="Profile Picture">
-        <div class="card-body">
-          <h5 class="card-title text-center">Name Lastname</h5>
-        </div>
-      </div>
-    </a>
-  </div>
-</div>
-<div class="row">
-  <div class="col-3">
-    <a href="#">
-      <div class="card">
-        <img src="images/profile_pic.png" class="card-img-top" alt="Profile Picture">
+        <img src="{{ asset('img/profile_pic.png')}}" class="card-img-top" alt="Profile Picture">
         <div class="card-body">
           <h5 class="card-title text-center">Name Lastname</h5>
         </div>
