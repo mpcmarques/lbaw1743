fdc927b8abf591e1b76121de4088735181a18a97
João Sá <up201506252@fe.up.pt>
2018-03-10 23:22:08 +0000

    project img css :gem:

--- website/resources/views/project/project_media.blade.php
+++ website/resources/views/project/project_media.blade.php
@@ -1,5 +1,5 @@
-<div class="media">
-  <img src="img/project_placeholder.png" class="mr-3" alt="Profile Picture" width="200">
+<div id="project_media" class="media">
+  <img src="img/project_placeholder.png" class="img-round" alt="Project Picture">
   <div class="media-body">
     <h2>Project name</h2>
     <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
