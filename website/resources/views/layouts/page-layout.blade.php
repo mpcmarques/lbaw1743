7cae66f3e35a3819577e093cb6f866fdaba69996
Mateus <mpcmarques@icloud.com>
2018-03-10 16:18:04 +0000

    common css

--- website/resources/views/layouts/page-layout.blade.php
+++ website/resources/views/layouts/page-layout.blade.php
@@ -8,7 +8,6 @@
     <link rel="shortcut icon" href="icons/logo32.ico">
 
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}"/>
-    <link rel="stylesheet" href="{{ asset('css/css.css') }}"/>
     <link rel="stylesheet" href= @yield('css') />
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
