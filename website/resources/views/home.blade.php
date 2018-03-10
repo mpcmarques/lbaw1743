0cc69c9a0a5df87f3872f639530e33ea20749371
João Sá <up201506252@fe.up.pt>
2018-03-10 14:43:43 +0000

    home css :gem:

--- website/resources/views/home.blade.php
+++ website/resources/views/home.blade.php
@@ -4,14 +4,13 @@
 
 @section('content')
 <!-- home page -->
-<div class="container home-page text-center">
-      <img class="logo" src="img/logo.png" alt="Plenum">
-      <hr/>
-      <div class="home-content">
-        <h1>Plenum</h1>
-        <p>
-          Welcome to your go to online project manager. Build your dream team, split your tasks and get to work!
-        </p>
-      </div>
+<div id="home-page" class="container">
+  <img class="logo" src="img/logo.png" alt="Plenum">
+  <div class="home-content">
+    <p class="text-center project-name">Plenum</p>
+    <p class="text-center description">
+      Welcome to your go to online project manager. Build your dream team, split your tasks and get to work!
+    </p>
+  </div>
 </div>
 @endsection
