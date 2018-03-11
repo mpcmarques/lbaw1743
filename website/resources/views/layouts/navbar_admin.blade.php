00ecce8e7288386d1d62873c8f1e29ba72933d1b
Bernardo Barbosa <up201503477@fe.up.pt>
2018-03-11 20:31:50 +0000

    improved admin navbar when logged in :gem:

--- website/resources/views/layouts/navbar_admin.blade.php
+++ website/resources/views/layouts/navbar_admin.blade.php
@@ -3,4 +3,14 @@
   <img  src="{{ asset('img/logo.png') }}" alt="Plenum" width="40" height="40"/>
   Administration
   </a>
+  <!-- if logged in -->
+  <div class="nav-item dropdown text-right">
+			<a class="nav-link dropdown-toggle" role="button" data-toggle="dropdown">
+        Logged in as
+        <span class="text-link" href="#">@admin</span>
+			</a>
+			<div class="dropdown-menu">
+				<a class="dropdown-item" href="/admin">Logout</a>
+			</div>
+	</div>
 </nav>
