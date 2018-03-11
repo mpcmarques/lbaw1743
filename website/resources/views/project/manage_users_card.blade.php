3887ec0babac5d6dd3dd44896370aaed0f8ad320
João Sá <up201506252@fe.up.pt>
2018-03-11 00:06:18 +0000

    project_manage_users :gem:

--- website/resources/views/project/manage_users_card.blade.php
+++ website/resources/views/project/manage_users_card.blade.php
@@ -32,16 +32,24 @@
         <th scope="col-1">
         </th>
         <th scope="col-3">
-          <p class="text-left font-weight-bold">Username</p>
+          <div class="text-left font-weight-bold">
+            Username
+          </div>
         </th>
         <th scope="col-3">
-          <p class="text-left font-weight-bold">Tasks assigned</p>
+          <div class="text-left font-weight-bold">
+            Tasks assigned
+          </div>
         </th>
         <th scope="col-2">
-          <p class="text-left font-weight-bold">Name</p>
+          <div class="text-left font-weight-bold">
+            Name
+          </div>
         </th>
         <th scope="col-3">
-          <p class="text-left font-weight-bold">Role</p>
+          <div class="text-left font-weight-bold">
+            Role
+          </div>
         </th>
       </tr>
     </thead>
@@ -50,39 +58,47 @@
         <th scope="row">
           <div class="checkbox">
             <label><input type="checkbox" value="">
-          </div>
-        </th>
-        <td>
-          <p class"text-left">jotapsa</p>
-        </td>
-        <td>
-          <p class="text-left">5</p>
-        </td>
-        <td>
-          <p class="text-left">João Sá</p>
-        </td>
-        <td>
-          <p class="text-left">Member</p>
-        </td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-@endsection
-
-@section('card-footer')
-
-<div class="card-footer">
-  <div class="float-right">
-    <a class="btn btn-terciary" href="#" role="button">
-      <span class="octicon octicon-pencil"/>
-      Edit
-    </a>
-    <a class="btn btn-primary" href="#" role="button">
-      <span class="octicon octicon-trashcan"/>
-      Remove
-    </a>
+            </div>
+          </th>
+          <td>
+            <div class="text-left">
+              jotapsa
+            </div>
+          </td>
+          <td>
+            <div class="text-left">
+              11
+            </div>
+          </td>
+          <td>
+            <div class="text-left">
+              João Sá
+            </div>
+          </td>
+          <td>
+            <div class="text-left">
+              Member
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
-</div>
+  @endsection
 
-@endsection
+  @section('card-footer')
+
+  <div class="card-footer">
+    <div class="float-right">
+      <a class="btn btn-terciary" href="#" role="button">
+        <span class="octicon octicon-pencil"/>
+        Edit
+      </a>
+      <a class="btn btn-primary" href="#" role="button">
+        <span class="octicon octicon-trashcan"/>
+        Remove
+      </a>
+    </div>
+  </div>
+
+  @endsection
