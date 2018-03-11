4040d5c95f68654127870ddf42da44d4daf3513d
Bernardo Barbosa <up201503477@fe.up.pt>
2018-03-11 21:14:52 +0000

    About Page :gem:

--- website/resources/views/about.blade.php
+++ website/resources/views/about.blade.php
@@ -6,6 +6,12 @@
 
 <div id="about-page" class="container about-page">
     <h1 class="text-center">About</h1>
+    <div class="container developers text-center">
+      @include('layouts.user-card')
+      @include('layouts.user-card')
+      @include('layouts.user-card')
+      @include('layouts.user-card')
+    </div>
     <div class="container text">
         <p class="text-center">
             Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum. Donec sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At nos hinc posthac, sitientis piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Cras mattis iudicium purus sit amet fermentum.
