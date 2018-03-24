5e3b5d832dfb5f0709ec8778e9efd1ea2215c98f
mariosantosprivate <mariosantos.private@gmail.com>
2018-03-24 18:12:42 +0000

    Added users

--- database/inserts.sql
+++ database/inserts.sql
@@ -1,4 +1,25 @@
 
+--Users
+insert into UserTable (userName, password, email, name, gender, address, institution, description, imgUrl) values ('gsignorelli0', 'eMQ40cPHVgr', 'gsignorelli0@cloudflare.com', 'Gloriana Signorelli', 'Female', '9065 Miller Avenue', 'Kayveo', '8/8/2017');
+insert into UserTable (userName, password, email, name, gender, address, institution, description, imgUrl) values ('aburdis1', 'eVruY8rB9us', 'aburdis1@linkedin.com', 'Ardine Burdis', 'Female', '12750 Heffernan Center', 'Brainsphere', '9/26/2017');
+insert into UserTable (userName, password, email, name, gender, address, institution, description, imgUrl) values ('ckerfut2', 'xiZtGm1', 'ckerfut2@intel.com', 'Cairistiona Kerfut', 'Female', '09 Cody Trail', 'Topicstorm', '11/26/2017');
+insert into UserTable (userName, password, email, name, gender, address, institution, description, imgUrl) values ('sduffet3', '6Jrygh', 'sduffet3@elegantthemes.com', 'Sheffy Duffet', 'Male', '71 Dottie Junction', 'Wikivu', '4/16/2017');
+insert into UserTable (userName, password, email, name, gender, address, institution, description, imgUrl) values ('llanger4', '34G1lulS7Bz', 'llanger4@google.co.uk', 'Lindsay Langer', 'Female', '20 David Street', 'Agimba', '1/1/2018');
+insert into UserTable (userName, password, email, name, gender, address, institution, description, imgUrl) values ('mbauer5', 'E4rkY8tnvt7', 'mbauer5@dion.ne.jp', 'Meredithe Bauer', 'Female', '90 Ridgeview Crossing', 'Topdrive', '11/1/2017');
+insert into UserTable (userName, password, email, name, gender, address, institution, description, imgUrl) values ('nwinkett6', 'tytfyuUdd', 'nwinkett6@dot.gov', 'Noel Winkett', 'Female', '9277 Schmedeman Junction', 'Zoombeat', '3/8/2018');
+insert into UserTable (userName, password, email, name, gender, address, institution, description, imgUrl) values ('anairn7', 'Tc2grOjvvRQB', 'anairn7@webnode.com', 'Agretha Nairn', 'Female', '0 2nd Place', 'Mybuzz', '5/16/2017');
+insert into UserTable (userName, password, email, name, gender, address, institution, description, imgUrl) values ('mvala8', 'yi4KQi', 'mvala8@fotki.com', 'Maria Vala', 'Female', '0636 Atwood Circle', 'Zazio', '8/23/2017');
+insert into UserTable (userName, password, email, name, gender, address, institution, description, imgUrl) values ('brappoport9', 'GGKIFksZmm', 'brappoport9@time.com', 'Bryana Rappoport', 'Female', '5311 Cody Court', 'Rhyloo', '12/1/2017');
+insert into UserTable (userName, password, email, name, gender, address, institution, description, imgUrl) values ('bwardalea', 'mjrQImo', 'bwardalea@accuweather.com', 'Blythe Wardale', 'Female', '4927 Magdeline Lane', 'Centizu', '9/1/2017');
+insert into UserTable (userName, password, email, name, gender, address, institution, description, imgUrl) values ('aeloyb', 'qcs2Rfl', 'aeloyb@google.com.br', 'Axel Eloy', 'Male', '72370 Orin Plaza', 'Realcube', '5/9/2017');
+insert into UserTable (userName, password, email, name, gender, address, institution, description, imgUrl) values ('bsmoughtonc', 'axi3uln', 'bsmoughtonc@rakuten.co.jp', 'Beverley Smoughton', 'Female', '10480 Village Junction', 'Quimm', '6/11/2017');
+insert into UserTable (userName, password, email, name, gender, address, institution, description, imgUrl) values ('lkibblewhited', 'MMNDkEZoYnp', 'lkibblewhited@hubpages.com', 'Liz Kibblewhite', 'Female', '81273 Southridge Plaza', 'Realfire', '6/10/2017');
+insert into UserTable (userName, password, email, name, gender, address, institution, description, imgUrl) values ('kgrigolone', 'WZvFapm3ukr', 'kgrigolone@goo.ne.jp', 'Kayla Grigolon', 'Female', '033 Springs Circle', 'Photobug', '7/24/2017');
+insert into UserTable (userName, password, email, name, gender, address, institution, description, imgUrl) values ('ymcf', 'UTBv8n', 'ymcf@etsy.com', 'Yulma Mc Ilwrick', 'Male', '86 Onsgard Street', 'Livetube', '12/7/2017');
+insert into UserTable (userName, password, email, name, gender, address, institution, description, imgUrl) values ('kdaunayg', 'L2SQz3', 'kdaunayg@wp.com', 'Ketty Daunay', 'Female', '08 Talisman Terrace', 'Devcast', '10/14/2017');
+insert into UserTable (userName, password, email, name, gender, address, institution, description, imgUrl) values ('dmunganh', 'Je4GrmjgHj', 'dmunganh@phoca.cz', 'Drucie Mungan', 'Female', '815 Del Sol Street', 'Aimbu', '8/7/2017');
+insert into UserTable (userName, password, email, name, gender, address, institution, description, imgUrl) values ('dpiochi', '03INlP', 'dpiochi@businessweek.com', 'Dorian Pioch', 'Male', '600 Buell Court', 'Eidel', '6/7/2017');
+insert into MOCK_DATA (userName, password, email, name, gender, address, institution, description, imgUrl) values ('tthorouggoodj', 'Oz114Lx', 'tthorouggoodj@rakuten.co.jp', 'Trueman Thorouggood', 'Male', '19 East Drive', 'Youspan');
 
 --Countries
 INSERT INTO plenum.country (name) VALUES ('Sweden');
