469a3059b7e7053745d20d2c2c512c047ee11c9b
mariosantosprivate <mariosantos.private@gmail.com>
2018-03-24 17:59:10 +0000

    Added countries

---
+++ database/inserts.sql
@@ -0,0 +1,23 @@
+
+
+--Countries
+INSERT INTO plenum.country (name) VALUES ('Sweden');
+INSERT INTO plenum.country (name) VALUES ('France');
+INSERT INTO plenum.country (name) VALUES ('Venezuela');
+INSERT INTO plenum.country (name) VALUES ('Montenegro');
+INSERT INTO plenum.country (name) VALUES ('Portugal');
+INSERT INTO plenum.country (name) VALUES ('Brazil');
+INSERT INTO plenum.country (name) VALUES ('China');
+INSERT INTO plenum.country (name) VALUES ('Kuwait');
+INSERT INTO plenum.country (name) VALUES ('Madagascar');
+INSERT INTO plenum.country (name) VALUES ('Russia');
+INSERT INTO plenum.country (name) VALUES ('United States');
+INSERT INTO plenum.country (name) VALUES ('Japan');
+INSERT INTO plenum.country (name) VALUES ('Spain');
+INSERT INTO plenum.country (name) VALUES ('Indonesia');
+INSERT INTO plenum.country (name) VALUES ('Canada');
+INSERT INTO plenum.country (name) VALUES ('Scotland');
+INSERT INTO plenum.country (name) VALUES ('China');
+INSERT INTO plenum.country (name) VALUES ('Afghanistan');
+INSERT INTO plenum.country (name) VALUES ('Mongolia');
+INSERT INTO plenum.country (name) VALUES ('Jamaica');
