0bf9e951b88dcea70f58fb4b63d720771b01c6d8
João Sá <up201506252@fe.up.pt>
2018-04-01 21:35:46 +0100

    triggers :bug:

--- database/triggers.sql
+++ database/triggers.sql
@@ -5,13 +5,10 @@ CREATE OR REPLACE FUNCTION plenum.insertPost() RETURNS TRIGGER AS
 $BODY$
   BEGIN
     IF NOT EXISTS (
-      SELECT Joined.idJoined FROM plenum.Joined
-      WHERE (new.idUser = idUser AND new.idProject = idProject))
+      SELECT * FROM plenum.Joined
+      WHERE new.idUser = Joined.idUser AND new.idProject = Joined.idProject)
     THEN
       RAISE EXCEPTION 'Only a user who joined a project can post in its forum.';
-    ELSE
-      INSERT INTO plenum.ForumPost(creationDate, lastEditDate, title, content, idProject, idUser)
-      VALUES (new.creationDate, new.lastEditDate, new.title, new.content, new.idProject, new.idUser);
     END IF;
     RETURN NEW;
   END
@@ -29,13 +26,10 @@ CREATE OR REPLACE FUNCTION plenum.insertReply() RETURNS TRIGGER AS
 $BODY$
   BEGIN
     IF NOT EXISTS (
-      SELECT idJoined FROM plenum.Joined, plenum.ForumPost
-      WHERE (new.idPost = ForumPost.idPost AND ForumPost.idProject = Joined.idProject AND new.idUser = Joined.idUser))
+      SELECT * FROM plenum.Joined, plenum.ForumPost
+      WHERE new.idPost = ForumPost.idPost AND ForumPost.idProject = Joined.idProject AND new.idUser = Joined.idUser)
     THEN
       RAISE EXCEPTION 'Only a user who joined a project can reply to posts in its forum.';
-    ELSE
-      INSERT INTO plenum.Reply(creationDate, lastEditDate, content, idPost, idUser)
-      VALUES (new.creationDate, new.lastEditDate, new.content, new.idPost, new.idUser);
     END IF;
     RETURN NEW;
   END
@@ -53,13 +47,10 @@ CREATE OR REPLACE FUNCTION plenum.insertComent() RETURNS TRIGGER AS
 $BODY$
   BEGIN
     IF NOT EXISTS (
-      SELECT idJoined FROM plenum.Joined, plenum.Task
-      WHERE (new.idTask = Task.idTask AND Task.idProject = Joined.idProject AND new.idUser = Joined.idUser))
+      SELECT * FROM plenum.Joined, plenum.Task
+      WHERE new.idTask = Task.idTask AND Task.idProject = Joined.idProject AND new.idUser = Joined.idUser)
     THEN
       RAISE EXCEPTION 'Only a user who joined a project can create comments for a Project Task.';
-    ELSE
-      INSERT INTO plenum.Comment(creationDate, lastEditDate, idTask, idUser, idParent)
-      VALUES (new.creationDate, new.lastEditDate, new.idTask, new.idUser, new.idParent);
     END IF;
     RETURN NEW;
   END;
@@ -78,12 +69,9 @@ $BODY$
   BEGIN
     IF NOT EXISTS (
       SELECT * FROM plenum.Assigned
-      WHERE (new.idTask = Assigned.idTask AND new.idUser = Assigned.idUser))
+      WHERE new.idTask = Assigned.idTask AND new.idUser = Assigned.idUser)
     THEN
       RAISE EXCEPTION 'Only a user who joined a project can create comments for a Project Task.';
-    ELSE
-      INSERT INTO plenum.Comment(creationDate, lastEditDate, idTask, idUser, idParent)
-      VALUES (new.creationDate, new.lastEditDate, new.idTask, new.idUser, new.idParent);
     END IF;
     RETURN NEW;
   END;
@@ -101,13 +89,10 @@ CREATE OR REPLACE FUNCTION plenum.insertTask() RETURNS TRIGGER AS
 $BODY$
   BEGIN
     IF NOT EXISTS (
-      SELECT Joined.idJoined FROM plenum.Joined
-      WHERE (new.idUser = Joined.idUser AND new.idProject = Joined.idProject))
+      SELECT * FROM plenum.Joined
+      WHERE new.idUser = Joined.idUser AND new.idProject = Joined.idProject)
     THEN
       RAISE EXCEPTION 'Only a user who joined a project can create tasks for it.';
-    ELSE
-      INSERT INTO plenum.Task(creationDate, lastEditDate, title, description, deadline, completed, completetionDate, idUser, idProject)
-      VALUES (new.creationDate, new.lastEditDate, new.title, new.description, new.deadline, new.completed, new.completetionDate, new.idUser, new.idProject);
     END IF;
     RETURN NEW;
   END;
@@ -125,13 +110,10 @@ CREATE OR REPLACE FUNCTION plenum.insertAssigned() RETURNS TRIGGER AS
 $BODY$
   BEGIN
     IF NOT EXISTS (
-      SELECT Joined.idJoined FROM plenum.Joined, plenum.Task
-      WHERE (new.idUser = Joined.idUser AND new.idTask = Task.idTask AND Task.idProject = Joined.idProject))
+      SELECT * FROM plenum.Joined, plenum.Task
+      WHERE new.idUser = Joined.idUser AND new.idTask = Task.idTask AND Task.idProject = Joined.idProject)
     THEN
       RAISE EXCEPTION 'Only a user who joined a project can be assigned to its tasks.';
-    ELSE
-      INSERT INTO plenum.Assigned(idUser, idTask)
-      VALUES (new.idUser, new.idTask);
     END IF;
     RETURN NEW;
   END;
@@ -153,9 +135,6 @@ $BODY$
     WHERE new.idProject = Joined.idProject AND Joined.role = 'Owner' AND Joined.idUser = UserTable.idUser AND UserTable.premium = true)
   THEN
     RAISE EXCEPTION 'A Project can only be private while the Owner is a Premium User.';
-  ELSE
-    INSERT INTO plenum.Project(idProject, creationDate, name, description, private)
-    VALUES (new.idProject, new.creationDate, new.name, new.description, new.private);
   END IF;
   RETURN NEW;
   END;
@@ -174,19 +153,16 @@ $BODY$
   BEGIN
   IF EXISTS (
     SELECT * FROM plenum.Joined, plenum.Project
-    WHERE new.idUser = Joined.idUser AND Joined.role = 'Owner' AND new.idProject = Joined.idProject AND new.idProject = Project.idProject AND Project.private = true)
+    WHERE new.idUser = Joined.idUser AND Joined.role = 'Owner' AND Project.idProject = Joined.idProject AND Project.private = true)
   THEN
     RAISE EXCEPTION 'A Project can only be private while the Owner is a Premium User.';
-  ELSE
-    INSERT INTO plenum.UserTable(username, password, email, premium, banned, name, gender, address, institution, description, birthDate, idCountry)
-    VALUES (new.username, new.password, new.email, new.premium, new.banned, new.name, new.gender, new.address, new.institution, new.description, new.birthDate, new.idCountry);
   END IF;
   RETURN NEW;
   END;
 $BODY$
 LANGUAGE plpgsql;
 
-CREATE TRIGGER onUpdateUser BEFORE INSERT OR UPDATE ON plenum.UserTable
+CREATE TRIGGER onUpdateUser BEFORE UPDATE ON plenum.UserTable
 FOR EACH ROW
 WHEN (new.premium = false)
 EXECUTE PROCEDURE plenum.updateUserTable();
