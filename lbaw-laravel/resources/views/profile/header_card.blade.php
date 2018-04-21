bffafdfa9019b9d3ba76f6c515e2be53061f9baa
João Sá <up201506252@fe.up.pt>
2018-04-22 00:20:03 +0100

    :bug: profile images

--- lbaw-laravel/resources/views/profile/header_card.blade.php
+++ lbaw-laravel/resources/views/profile/header_card.blade.php
@@ -8,8 +8,8 @@
     <span class="octicon octicon-pencil">
     </span>
   </button>
-  
-  
+
+
   <div class="modal fade bd-example-modal-lg" id="editprofile-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
       <div class="modal-content">
@@ -19,19 +19,19 @@
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        
+
         <div class="modal-body mx-3">
-          
+
           <div class="md-form mb-3 required">
             <label for="inputName" class="col-sm-4 col-form-label">Full Name</label>
             <input type="text" class="form-control" value="Laboratório de Bases de Dados e Aplicações Web" required>
           </div>
-          
+
           <div class="md-form mb-3 required">
             <label for="inputName" class="col-sm-4 col-form-label">Username</label>
             <input type="text" class="form-control" value="lbaw" required>
           </div>
-          
+
           <div class="md-form mb-3 required">
             <label for="inputName" class="col-sm-4 col-form-label">Email</label>
             <input type="text" class="form-control" value="lbaw@fe.up.pt" required>
@@ -39,19 +39,19 @@
               We'll never share your email with anyone else.
             </small>
           </div>
-          
+
           <div class="md-form mb-3 required">
             <label for="inputName" class="col-sm-4 col-form-label">Password</label>
             <input type="password" class="form-control" value="********" required>
           </div>
-          
+
           <div class="md-form mb-3 required">
             <label for="inputName" class="col-sm-6 col-form-label">Repeat Password</label>
             <input type="password" class="form-control" value="********" required>
           </div>
-          
+
           <!--  Gender -->
-          
+
           <div class="md-form mb-3">
             <label for="inputName" class="col-sm-6 col-form-label">Institucion / Company</label>
             <input type="text" class="form-control" value="FEUP">
@@ -59,12 +59,12 @@
               Fill if you belong to a company or institution.
             </small>
           </div>
-          
+
           <div class="md-form mb-3 required">
             <label for="inputName" class="col-sm-4 col-form-label">Birthday</label>
             <input type="date" class="form-control" value="2018-03-01" required>
           </div>
-          
+
           <div class="md-form mb-3">
             <label for="inputName" class="col-sm-4 col-form-label">Profile Picture</label>
             <div class="custom-file">
@@ -72,12 +72,12 @@
               <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
             </div>
           </div>
-          
+
           <div class="modal-footer">
             <button type="submit" class="btn btn-primary">Save Changes</button>
           </ul>
         </div>
-        
+
       </div>
     </div>
   </div>
@@ -90,7 +90,7 @@
   <div class="grid">
     <div class="row">
       <div class="col-md-3 text-center">
-        <img class="img-round" src="{{ asset('img/profile_pic.png') }}" alt="Profile Picture" width="100">
+        <img class="img-round" src="{{ asset('img/profile/'.$profile->iduser.'.png') }}" alt="Profile Picture" width="100">
       </div>
       <div class="col-md-9">
         <h1 class="display-4">{{$profile->username}}</h1>
@@ -109,5 +109,3 @@
 @endif
 
 </div>
-
-
