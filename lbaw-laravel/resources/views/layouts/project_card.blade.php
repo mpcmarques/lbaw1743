ca120072f436c0faa695d2249c7bb40647f49604
Mateus <mpcmarques@icloud.com>
2018-04-21 17:19:42 +0100

    fix projects card design :bug:

--- lbaw-laravel/resources/views/layouts/project_card.blade.php
+++ lbaw-laravel/resources/views/layouts/project_card.blade.php
@@ -7,16 +7,15 @@
                 <span  class="octicon octicon-clippy"></span>
             </a>
             
-            <button type="button" class="btn btn-secondary">
-                <a href="{{ url('project_forum')}}">
-                    <span  class="octicon octicon-comment-discussion"></span>
-                </a>
-            </button>
-            <button type="button" class="btn btn-primary">
-                <a href="{{ url('project_options') }}">
-                    <span  class="octicon octicon-tools"></span>
-                </a>
-            </button>
+            <a href="{{ url('project/'.$project->idproject.'/forum')}}"  class="btn btn-secondary">
+                <span  class="octicon octicon-comment-discussion"></span>
+            </a>
+            
+            
+            <a href="{{ url('project/'.$project->idproject.'/options') }}" class="btn btn-primary">
+                <span  class="octicon octicon-tools"></span>
+            </a>
+            
         </div>
     </div>
 </div>
