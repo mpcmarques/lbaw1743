88f290aba38352a653e52a642704744e09b382bb
Bernardo Barbosa <up201503477@fe.up.pt>
2018-04-30 17:13:35 +0100

    Fixed seed.sql encrypted passwords :ambulance:

--- lbaw-laravel/resources/views/errors/404.blade.php
+++ lbaw-laravel/resources/views/errors/404.blade.php
@@ -11,7 +11,7 @@
           <h1 class="text-right">4</h1>
         </div>
         <div class="col">
-          <a href="index.php">
+          <a href="/">
             <img src="{{ asset('img/logo.png') }}">
           </a>
         </div>
--- lbaw-laravel/resources/views/layouts/navbar_admin.blade.php
+++ lbaw-laravel/resources/views/layouts/navbar_admin.blade.php
@@ -3,14 +3,18 @@
   <img  src="{{ asset('img/logo.png') }}" alt="Plenum" width="40" height="40"/>
   Administration
   </a>
-  <!-- if logged in -->
+
+  @if(Auth::check())
+
   <div class="nav-item dropdown text-right">
-			<a class="nav-link dropdown-toggle" role="button" data-toggle="dropdown">
-        Logged in as
-        <span class="text-link" href="#">@admin</span>
-			</a>
-			<div class="dropdown-menu">
-				<a class="dropdown-item" href="/admin">Logout</a>
-			</div>
-	</div>
+      <a class="nav-link dropdown-toggle" role="button" data-toggle="dropdown">
+        Logged as
+        <span class="text-link" href="#">{{Auth::user()->username}}</span>
+      </a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href=href="{{ url('/admin/logout') }}">Logout</a>
+      </div>
+  </div>
+
+  @endif
 </nav>
