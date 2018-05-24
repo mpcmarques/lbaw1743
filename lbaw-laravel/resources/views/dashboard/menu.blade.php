3fc5592e0ffc4541ffb890b5ef2b1a5df0018a59
Bernardo Barbosa <up201503477@fe.up.pt>
2018-05-24 22:37:28 +0100

    dashboard menu :gem:

--- lbaw-laravel/resources/views/dashboard/menu.blade.php
+++ lbaw-laravel/resources/views/dashboard/menu.blade.php
@@ -1,12 +1,23 @@
-
 <div class="list-group">
+  @if ( $_SERVER['PATH_INFO'] == '/dashboard')
+  <a href="{{ url('dashboard') }}" class="list-group-item list-group-item-action active" role="button">
+  @else
   <a href="{{ url('dashboard') }}" class="list-group-item list-group-item-action" role="button">
+  @endif
     Dashboard
   </a>
+  @if ( $_SERVER['PATH_INFO'] == '/dashboard/projects')
+  <a href="{{ url('dashboard/projects') }}" class="list-group-item list-group-item-action active" role="button">
+  @else
   <a href="{{ url('dashboard/projects') }}" class="list-group-item list-group-item-action" role="button">
+  @endif
     My Projects
   </a>
+  @if ( $_SERVER['PATH_INFO'] == '/dashboard/tasks')
+  <a href="{{ url('dashboard/tasks') }}" class="list-group-item list-group-item-action active" role="button">
+  @else
   <a href="{{ url('dashboard/tasks') }}" class="list-group-item list-group-item-action" role="button">
+  @endif
     Tasks
   </a>
 </div>
