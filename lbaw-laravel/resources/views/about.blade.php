8e1d04cda7b10fea5a5523487d1e29ce29e4eb94
Bernardo Barbosa <up201503477@fe.up.pt>
2018-05-29 12:37:58 +0100

    github links in about :gem:

--- lbaw-laravel/resources/views/about.blade.php
+++ lbaw-laravel/resources/views/about.blade.php
@@ -15,28 +15,36 @@
         <div class="container developers text-center">
 
               <div class="card user-card">
+                <a href="https://github.com/mpcmarques">
                   <img src="https://avatars3.githubusercontent.com/u/15067361?s=400&v=4" class="card-img-top" alt="Profile Picture">
+                </a>
                   <div class="card-body">
                       <h5 class="card-title text-center">Mateus Pedroza</h5>
                   </div>
               </div>
 
               <div class="card user-card">
+                <a href="https://github.com/jotapsa">
                   <img src="https://avatars2.githubusercontent.com/u/8916652?s=460&v=4" class="card-img-top" alt="Profile Picture">
+                </a>
                   <div class="card-body">
                       <h5 class="card-title text-center">João Sá</h5>
                   </div>
               </div>
 
               <div class="card user-card">
+                <a href="https://github.com/bernardomcbarbosa">
                   <img src="https://avatars2.githubusercontent.com/u/25747760?s=460&v=4" class="card-img-top" alt="Profile Picture">
+                </a>
                   <div class="card-body">
-                      <h5 class="card-title text-center">Bernardo Costa</h5>
+                      <h5 class="card-title text-center">Bernardo Barbosa</h5>
                   </div>
               </div>
 
               <div class="card user-card">
+                <a href="https://github.com/mariosantosprivate">
                   <img src="https://avatars2.githubusercontent.com/u/25696878?s=460&v=4" class="card-img-top" alt="Profile Picture">
+                </a>
                   <div class="card-body">
                       <h5 class="card-title text-center">Mário Santos</h5>
                   </div>
